291f566240fe09b11ce71ef7a0022573a60da234
chenlong <tom.chen@example.com>
2020-06-26 22:36:56 +0800

    .

--- run_ios.sh
+++ run_ios.sh
@@ -1,1 +1,4 @@
+# image lookup -vn <SomeFunctionInTheLibrary>
+# settings set target.source-map /Users/user/Ctsmed/mediapipe/external/org_tensorflow/tensorflow/lite/delegates/gpu/metal/compute_task.mm /private/var/tmp/_bazel_chenlong/46cfe12d82525b8b2b43cf38d2da2eb7/external/org_tensorflow/tensorflow/lite/delegates/gpu/metal/compute_task.mm
+
 bazel build -c opt --config=ios_arm64 //mediapipe/examples/ios/hededgedetectiongpu:HedEdgeDetectionApp --verbose_failures
